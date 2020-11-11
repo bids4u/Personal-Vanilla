5e990d3348301f80ed9f6d638f9cd4bc994166f2
bids4u <38151195+bids4u@users.noreply.github.com>
2020-11-11 10:00:56 +0545

    Update index.js
    name added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function validateForm() {
-    var name = document.forms["myForm"]["email"].value.trim();
+    var name = document.forms["myForm"]["name"].value.trim();
+    var email = document.forms["myForm"]["email"].value.trim();
     var comments = document.forms["myForm"]["comments"].value.trim();
     if (name == "") {
       alert("Name must be filled out");
+      return false;
+    }
+    else if (email == "") {
+      alert("Email must be filled out");
       return false;
     }
     else if (comments == "") {
